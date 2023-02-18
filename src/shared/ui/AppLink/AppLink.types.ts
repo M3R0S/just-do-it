e7ba836cc82a4d8ef7e754260782a05ace82b47c
alone <bogdan_yalta@mail.ru>
2023-02-18 19:36:30 +0300

    fix types AppLink

--- src/shared/ui/AppLink/AppLink.types.ts
+++ src/shared/ui/AppLink/AppLink.types.ts
@@ -3,7 +3,9 @@ import { ValueOf } from "shared/lib/types/helpersTypes";
 
 export const AppLinkTheme = {
     PRIMARY: "primary",
+    PRIMARY_INVERTED: "primary-inverted",
     SECONDARY: "secondary",
+    SECONDARY_INVERTED: "secondary-inverted",
 } as const;
 
 export type AppLinkTheme = ValueOf<typeof AppLinkTheme>;
