a6fd7fd960dc440f29fd7c16f98266fd79aa6a23
alone <bogdan_yalta@mail.ru>
2023-02-18 21:07:04 +0300

    #10

--- src/app/providers/theme/ui/ThemeProvider.tsx
+++ src/app/providers/theme/ui/ThemeProvider.tsx
@@ -3,33 +3,45 @@ import { FC, useMemo, useState } from "react";
 import { ThemeProviderProps } from "./ThemeProvider.types";
 
 import {
-    LOCAL_STORAGE_THEME_KEY,
-    Theme,
+    // LOCAL_STORAGE_THEME_KEY,
+    // Theme,
     ThemeContext,
-    ThemeContextProps,
+    // ThemeContextProps,
 } from "shared/lib/context/ThemeContext";
 
-const defaultTheme =
-    (localStorage.getItem(LOCAL_STORAGE_THEME_KEY) as Theme) || Theme.LIGHT;
+// const defaultTheme =
+//     (localStorage.getItem(LOCAL_STORAGE_THEME_KEY) as Theme) || Theme.LIGHT;
+
+// const ThemeProvider: FC<ThemeProviderProps> = (props) => {
+//     const { children } = props;
+
+//     const [theme, setTheme] = useState<Theme>(defaultTheme);
+
+//     const defaultProviderValue = useMemo<ThemeContextProps>(
+//         () => ({
+//             theme,
+//             setTheme,
+//         }),
+//         [theme]
+//     );
+
+//     return (
+//         <ThemeContext.Provider value={defaultProviderValue}>
+//             {children}
+//         </ThemeContext.Provider>
+//     );
+// };
+
+// export default ThemeProvider
 
 const ThemeProvider: FC<ThemeProviderProps> = (props) => {
     const { children } = props;
 
-    const [theme, setTheme] = useState<Theme>(defaultTheme);
-
-    const defaultProviderValue = useMemo<ThemeContextProps>(
-        () => ({
-            theme,
-            setTheme,
-        }),
-        [theme]
-    );
-
     return (
-        <ThemeContext.Provider value={defaultProviderValue}>
+        <ThemeContext.Provider value={"value"}>
             {children}
         </ThemeContext.Provider>
     );
 };
 
-export default ThemeProvider
+export default ThemeProvider;
--- src/shared/lib/context/ThemeContext/ThemeContext.ts
+++ src/shared/lib/context/ThemeContext/ThemeContext.ts
@@ -1,9 +1,11 @@
 import { createContext } from "react";
 
-import { Theme, ThemeContextProps } from "./ThemeContext.types";
+// import { Theme, ThemeContextProps } from "./ThemeContext.types";
 
-export const ThemeContext = createContext({});
+// export const ThemeContext = createContext({});
 
-export const LOCAL_STORAGE_THEME_KEY = "theme";
+// export const LOCAL_STORAGE_THEME_KEY = "theme";
 
-//todo Вынести контекст в редакс
+// //todo Вынести контекст в редакс
+
+export const ThemeContext = createContext("value");
