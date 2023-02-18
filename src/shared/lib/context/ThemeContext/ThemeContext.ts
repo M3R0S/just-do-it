07b39fce9ffcc81f4dca11762bd960baefd2982b
alone <bogdan_yalta@mail.ru>
2023-02-18 20:38:58 +0300

    #6

--- src/shared/lib/context/ThemeContext/ThemeContext.ts
+++ src/shared/lib/context/ThemeContext/ThemeContext.ts
@@ -2,7 +2,10 @@ import { createContext } from "react";
 
 import { Theme, ThemeContextProps } from "./ThemeContext.types";
 
-export const ThemeContext = createContext<ThemeContextProps>({});
+export const ThemeContext = createContext<ThemeContextProps>({
+    theme: Theme.LIGHT,
+    setTheme: () => ({}),
+});
 
 export const LOCAL_STORAGE_THEME_KEY = "theme";
 
