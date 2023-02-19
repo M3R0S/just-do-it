8d63177e6697ce7dda910b2589406847829e62a1
Bogdan <bogdan_yalta@mail.ru>
2023-02-19 17:52:13 +0300

    work ubuntu

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,4 +1,4 @@
-import { FC, Suspense } from "react";
+import { FC, Suspense, useMemo, useState } from "react";
 
 import "./App.scss";
 import { AppRouter } from "./providers/Router";
@@ -6,24 +6,41 @@ import { AppRouter } from "./providers/Router";
 import { Navbar } from "widgets/Navbar";
 import { Sidebar } from "widgets/Sidebar";
 import { AppErrorBoundaryFallback } from "widgets/AppErrorBoundaryFallback";
-import { useTheme } from "shared/lib/hooks";
 import { cln } from "shared/lib/helpers";
 import { ErrorBoundary, Loader } from "shared/ui";
+import {
+    LOCAL_STORAGE_THEME_KEY,
+    Theme,
+    ThemeContext,
+} from "shared/lib/context/ThemeContext";
+
+const defaultTheme =
+    (localStorage.getItem(LOCAL_STORAGE_THEME_KEY) as Theme) || Theme.LIGHT;
 
 export const App: FC = () => {
-    const { theme } = useTheme();
+    const [theme, setTheme] = useState<Theme>(defaultTheme);
+
+    const defaultProps = useMemo(
+        () => ({
+            theme,
+            setTheme,
+        }),
+        [theme]
+    );
 
     return (
-        <div className={cln("app", [theme])}>
-            <Suspense fallback={<Loader className="app__loader" />}>
-                <ErrorBoundary fallback={<AppErrorBoundaryFallback />}>
-                    <Navbar />
-                    <div className="app__content_page">
-                        <Sidebar />
-                        <AppRouter />
-                    </div>
-                </ErrorBoundary>
-            </Suspense>
-        </div>
+        <ThemeContext.Provider value={{ theme, setTheme }}>
+            <div className={cln("app", [theme])}>
+                <Suspense fallback={<Loader className="app__loader" />}>
+                    <ErrorBoundary fallback={<AppErrorBoundaryFallback />}>
+                        <Navbar />
+                        <div className="app__content_page">
+                            <Sidebar />
+                            <AppRouter />
+                        </div>
+                    </ErrorBoundary>
+                </Suspense>
+            </div>
+        </ThemeContext.Provider>
     );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import { render } from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 
 import { App } from "./app/App";
-import { ThemeProvider } from "app/providers/Theme";
 
 import "shared/config/i18n/i18n";
 import { AppErrorBoundaryFallback } from "widgets/AppErrorBoundaryFallback";
@@ -11,9 +10,7 @@ import { ErrorBoundary } from "shared/ui";
 render(
     <BrowserRouter>
         <ErrorBoundary fallback={<AppErrorBoundaryFallback />}>
-            <ThemeProvider>
                 <App />
-            </ThemeProvider>
         </ErrorBoundary>
     </BrowserRouter>,
     document.getElementById("root")
