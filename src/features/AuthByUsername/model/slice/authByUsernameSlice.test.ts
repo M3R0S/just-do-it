2e893361950acaf72f12b82976d51d421dde19a2
Bogdan <bogdan_yalta@mail.ru>
2023-02-28 18:13:02 +0300

    fix unit test

--- src/features/AuthByUsername/model/slice/authByUsernameSlice.test.ts
+++
@@ -1,9 +0,0 @@
-// import { authByUsernameActions, authByUsernameSlice } from "./authByUsernameSlice";
-
-// describe("authByUsernameSlice", () => {
-//     test("should work with empty state", () => {
-//         expect(
-//             authByUsernameSlice.reducer(undefined, authByUsernameActions.))
-//         ).toEqual({});
-//     });
-// });
--- src/shared/ui/Overlay/Overlay.test.tsx
+++ src/shared/ui/Overlay/Overlay.test.tsx
@@ -1,5 +1,4 @@
 import { screen } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
 
 import { Overlay } from "./Overlay";
 
@@ -16,16 +15,4 @@ describe("Overlay", () => {
         const overlay = screen.getByTestId("overlay");
         expect(overlay).toBeInTheDocument();
     });
-
-    // test("closed overlay with btn", () => {
-    //     RenderWithComponent(
-    //         <Overlay
-    //             isActive={true}
-    //             onClose={() => ({})}
-    //         />
-    //     );
-    //     const overlay = screen.getByTestId("overlay");
-    //     userEvent.click(overlay);
-    //     expect(overlay).not.toBeInTheDocument();
-    // });
 });
